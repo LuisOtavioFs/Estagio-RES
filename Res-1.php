c54a6108d572b987acb0cb96b9d2ee3645f667e9
LuisOtavioFs <luisotaviofrsantos@gmail.com>
2024-03-07 14:06:24 -0300

    Comentario com a resposta adcionado

--- Res-1.php
+++ Res-1.php
@@ -14,21 +14,5 @@ while ($K < $indice) {
 echo"$soma";
 
 
-
-
-
+// Resposta é 91
 ?>
-
-<!-- int INDICE = 13, SOMA = 0, K = 0;
-
-enquanto K < INDICE faça
-
-{
-
-K = K + 1;
-
-SOMA = SOMA + K;
-
-}
-
-imprimir(SOMA); -->
