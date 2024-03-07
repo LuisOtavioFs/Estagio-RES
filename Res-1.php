5b579e3a4a2d351447faac367dc6d3f826ccdf9c
LuisOtavioFs <luisotaviofrsantos@gmail.com>
2024-03-07 13:20:07 -0300

    Resposta 1

---
+++ Res-1.php
@@ -0,0 +1,34 @@
+<?php 
+
+$indice = 13;
+$soma = 0;
+$K =0;
+
+while ($K < $indice) {
+    
+    $K = $K + 1;
+
+    $soma = $soma + $K;
+};
+
+echo"$soma";
+
+
+
+
+
+?>
+
+<!-- int INDICE = 13, SOMA = 0, K = 0;
+
+enquanto K < INDICE faça
+
+{
+
+K = K + 1;
+
+SOMA = SOMA + K;
+
+}
+
+imprimir(SOMA); -->
